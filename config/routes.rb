872b91c737f9fe4322e27fac4fd01955a69a7da3
Rebecca Wollard <rbwollard@gmail.com>
2023-03-14 12:00:55 -0600

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
